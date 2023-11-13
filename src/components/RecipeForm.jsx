1c1da084860056a78fa1a45d12628cf2c6cdb4aa
Your Name <einsteinkibet0110@gmail.com>
2023-11-13 08:38:07 +0300

    completed code project phase 2

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -1,7 +1,7 @@
-// RecipeForm.js
 import React, { useState } from 'react';
 
-const RecipeForm = () => {
+const RecipeForm = ({ onSaveRecipe }) => {
+  const [id, setId] = useState('');
   const [title, setTitle] = useState('');
   const [ingredients, setIngredients] = useState('');
   const [instructions, setInstructions] = useState('');
@@ -9,41 +9,73 @@ const RecipeForm = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
-    // Make a POST request to add a new recipe
-    const response = await fetch('http://localhost:3001/recipes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        ingredients: ingredients.split(',').map(ingredient => ingredient.trim()), // Split ingredients into an array
-        instructions,
-      }),
-    });
+    // Create a new recipe object
+    const newRecipe = {
+      id,
+      title,
+      ingredients,
+      instructions,
+    };
 
-    if (response.ok) {
-      // Recipe added successfully, you might want to redirect or update the UI
-      console.log('Recipe added successfully!');
-    } else {
-      // Handle error
-      console.error('Failed to add recipe.');
+    // Send a POST request to the server
+    try {
+      const response = await fetch('http://localhost:3000/recipes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newRecipe),
+      });
+
+      if (response.ok) {
+        // If the POST request is successful, update the state or perform any other necessary actions
+        onSaveRecipe(newRecipe);
+
+        // Clear the form fields
+        setId('');
+        setTitle('');
+        setIngredients('');
+        setInstructions('');
+      } else {
+        console.error('Failed to add recipe:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error adding recipe:', error);
     }
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <label>Title:</label>
-      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+    <div>
+      <h2>Add Recipe</h2>
+      <form onSubmit={handleFormSubmit}>
+        {/* Add input fields for title, ingredients, and instructions */}
+        <label>id:</label>
+        <textarea
+          value={id}
+          onChange={(e) => setIngredients(e.target.value)}
+        />
+        <label>Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        
+        <label>Ingredients:</label>
+        <textarea
+          value={ingredients}
+          onChange={(e) => setIngredients(e.target.value)}
+        />
 
-      <label>Ingredients:</label>
-      <input type="text" value={ingredients} onChange={(e) => setIngredients(e.target.value)} required />
+        <label>Instructions:</label>
+        <textarea
+          value={instructions}
+          onChange={(e) => setInstructions(e.target.value)}
+        />
 
-      <label>Instructions:</label>
-      <textarea value={instructions} onChange={(e) => setInstructions(e.target.value)} required />
-
-      <button type="submit">Add Recipe</button>
-    </form>
+        <button type="submit">Add Recipe</button>
+      </form>
+    </div>
   );
 };
 
